a64235aa4e8693abe4cf463553b8fbb10c83f7ee
IreneGMontoya <79455820+IreneGMontoya@users.noreply.github.com>
2021-04-13 12:04:07 +0200

    inicio pag web

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="Geany 1.25" />
+<link rel="stylesheet" href="cssindex.css">
+<title>MountAIn Main</title>
+</head>
+<!-- Empezamos -->
+<body>
+<div id="mainhome">
+    <div id="m_home_header">
+        <img src = "logoISE.png">
+        <img src = "logoISE.png">
+        <p></p>
+        <button  type="button" onclick="window.location.href= 'index.php'"><p>Home</p></button>
+        <button  type="button" onclick="window.location.href= 'aboutus.php'"><p>About Us</p></button>
+        <button  type="button" onclick="window.location.href= 'prediction/prediction.php'"><p>Prediction</p></button>
+        <button  type="button" onclick="window.location.href= 'clustering/clustering.php'"><p>Clustering</p></button>
+        <button  type="button" onclick="window.location.href= 'https://app.powerbi.com/view?r=eyJrIjoiMjdlNmFhOGMtNDRjOC00YWQ4LWJlMjctNjM5ODNjMGMyNzRjIiwidCI6IjZhZmVhODVkLWMzMjMtNDI3MC1iNjlkLWE0ZmIzOTI3YzI1NCIsImMiOjl9'"><p>Data Source</p></button>
+
+    </div>    
+    <div id="m_home_body_imagen">
+            <img src = "moodboard.png">
+    </div>
+    
+    <div id="m_home_prediction">
+        <button  type="button" onclick="window.location.href= 'prediction/prediction.php'"><img src="logoISE.png"><p>Prediction</p></button>
+    </div>
+
+    <div id="m_home_clustering">
+        <button  type="button" onclick="window.location.href= 'clustering/clustering.php'"><img src="logoISE.png"><p>Clustering</p></button>
+    </div>
+
+    <div id="m_home_body_moodboard">
+            <img src = "moodboard.png">
+    </div>
+
+    <footer>
+        <img src = "logoUPM.png">
+        <img src = "logoETSII.png">
+        <p>hola hola</p>
+    </footer>
+</div>
+</body>
+
+
+</html>
+
